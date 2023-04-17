606d6abd70d6909d484d2ededa1a4c9eb444fd23
Bùi Hữu Lộc <feature451@gmail.com>
2023-04-17 18:21:33 +0700

    fix: orderbydesc created at manage customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,17 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Mails\HelloUser;
 use App\Models\User;
-use Libraries\Request\Request;
-
 
 class CustomerController extends Controller
 {
 
     public function index()
     {
-        $customers = (new User)->where('is_admin', 0)->paginate(5);
+        $customers = (new User)->where('is_admin', 0)->orderByDesc('created_at')->paginate(5);
 
         return view('admin.customer.index', [
             'customers' => $customers,
