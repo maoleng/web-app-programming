a4367c94b235a333642403fb2cb9a0664c57dae7
Bùi Hữu Lộc <feature451@gmail.com>
2023-04-19 19:23:53 +0700

    feat: base ui customer

---
+++ app/Views/customer-theme/header.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" id="sectionsNav">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="presentation.html">THE CEENEMA</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="index.html">
+                        <i class="material-icons">apps</i> Now Showing Movie
+                    </a>
+                </li>
+                <li>
+                    <a href="index.html">
+                        <i class="material-icons">apps</i> Coming Soon Movie
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="material-icons">view_carousel</i> Hello Bui Huu Loc
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu dropdown-with-icons">
+                        <li>
+                            <a href="examples/landing-page.html">
+                                <i class="material-icons">view_day</i> Profile
+                            </a>
+                        </li>
+                        <li>
+                            <a href="examples/blog-post.html">
+                                <i class="material-icons">art_track</i> Cart
+                            </a>
+                        </li>
+                        <li>
+                            <a href="examples/blog-posts.html">
+                                <i class="material-icons">view_quilt</i> Order History
+                            </a>
+                        </li>
+                        <li>
+                            <a href="examples/about-us.html">
+                                <i class="material-icons">account_balance</i> Log out
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="button-container">
+                    <a href="" target="_blank" class="btn btn-white btn-round">
+                        <i class="material-icons">shopping_cart</i> Login
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ app/Views/customer/index.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html lang="en">
+<?php view('customer-theme.head_tag') ?>
+<body class="section-white">
+<?php view('customer-theme.header') ?>
+<div class="header-3">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <div class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="page-header" style="background-image: url('https://www.cgv.vn/media/banner/cache/1/b58515f018eb873dafa430b6f9ae0c1e/9/8/980x448_205.jpg');">
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="page-header" style="background-image: url('https://www.cgv.vn/media/banner/cache/1/b58515f018eb873dafa430b6f9ae0c1e/c/g/cgv-q2_980x448_1.jpg');">
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="page-header" style="background-image: url('https://www.cgv.vn/media/banner/cache/1/b58515f018eb873dafa430b6f9ae0c1e/9/8/980wx448h_27.jpg');">
+                    </div>
+                </div>
+            </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                <i class="material-icons">keyboard_arrow_left</i>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                <i class="material-icons">keyboard_arrow_right</i>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="cd-section" id="features">
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2 class="title">MOVIE SECTION</h2>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-product card-plain">
+                        <div class="card-image">
+                            <a href="#pablo">
+                                <img src="https://www.cgv.vn/media/catalog/product/cache/1/thumbnail/240x388/c88460ec71d04fa96e628a21494d2fd3/p/o/pope_sexorcist_poster_h_m_ng_c.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div class="card-content">
+                            <h4 class="card-title">
+                                <a href="#pablo">Gucci</a>
+                            </h4>
+                            <div class="footer text-center">
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-product card-plain">
+                        <div class="card-image">
+                            <a href="#pablo">
+                                <img src="https://www.cgv.vn/media/catalog/product/cache/1/thumbnail/240x388/c88460ec71d04fa96e628a21494d2fd3/p/o/pope_sexorcist_poster_h_m_ng_c.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div class="card-content">
+                            <h4 class="card-title">
+                                <a href="#pablo">Gucci</a>
+                            </h4>
+                            <div class="footer text-center">
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-product card-plain">
+                        <div class="card-image">
+                            <a href="#pablo">
+                                <img src="https://www.cgv.vn/media/catalog/product/cache/1/thumbnail/240x388/c88460ec71d04fa96e628a21494d2fd3/p/o/pope_sexorcist_poster_h_m_ng_c.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div class="card-content">
+                            <h4 class="card-title">
+                                <a href="#pablo">Gucci</a>
+                            </h4>
+                            <div class="footer text-center">
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-product card-plain">
+                        <div class="card-image">
+                            <a href="#pablo">
+                                <img src="https://www.cgv.vn/media/catalog/product/cache/1/thumbnail/240x388/c88460ec71d04fa96e628a21494d2fd3/p/o/pope_sexorcist_poster_h_m_ng_c.jpg" alt="" />
+                            </a>
+                        </div>
+                        <div class="card-content">
+                            <h4 class="card-title">
+                                <a href="#pablo">Gucci</a>
+                            </h4>
+                            <div class="footer text-center">
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                                <a href="#pablo" class="btn btn-rose btn-round">
+                                    <i class="material-icons">subject</i> Read
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 text-center">
+                    <button class="btn btn-primary btn-round">
+                        View more
+                        <div class="ripple-container"></div>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- section -->
+    <div class="section" id="carousel">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2 class="title">Why THE CEENEMA is the best</h2>
+                    <h5 class="description">Inspired by the business class seats on the plane, the GOLD CLASS theater was born to bring a luxurious and classy movie space to moviegoers. GOLD CLASS is also the perfect choice for special events to create wonderful, unforgettable memories.</h5>
+                </div>
+                <div class="col-md-8 col-md-offset-2">
+                    <!-- Carousel Card -->
+                    <div class="card card-raised card-carousel">
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <div class="carousel slide" data-ride="carousel">
+                                <!-- Indicators -->
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                </ol>
+                                <!-- Wrapper for slides -->
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img src="public/assets/img/bg2.jpg" alt="Awesome Image">
+                                        <div class="carousel-caption">
+                                            <h4><i class="material-icons">location_on</i> Yellowstone National Park, United States</h4>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="public/assets/img/bg3.jpg" alt="Awesome Image">
+                                        <div class="carousel-caption">
+                                            <h4><i class="material-icons">location_on</i> Somewhere Beyond, United States</h4>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="public/assets/img/bg.jpg" alt="Awesome Image">
+                                        <div class="carousel-caption">
+                                            <h4><i class="material-icons">location_on</i> Yellowstone National Park, United States</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Controls -->
+                                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                    <i class="material-icons">keyboard_arrow_left</i>
+                                </a>
+                                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                    <i class="material-icons">keyboard_arrow_right</i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Carousel Card -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php view('customer-theme.footer') ?>
+</body>
+<?php view('customer-theme.script') ?>
+</html>
