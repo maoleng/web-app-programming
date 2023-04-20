6294b8f78fbdf0a1bc11c3a3161e747b2cbdb2d8
Bùi Hữu Lộc <feature451@gmail.com>
2023-04-20 13:26:00 +0700

    feat: btn payment

--- app/Views/customer/order/choose_combo.php
+++ app/Views/customer/order/choose_combo.php
@@ -44,54 +44,15 @@
 </div>
 <div class="text-center">
     <a style="" href="<?= url('order/choose_seat') ?>" class="btn btn-info btn-round">Back<div class="ripple-container"></div></a>
-    <a style="" href="#pablo" class="btn btn-success btn-round">Pay<div class="ripple-container"></div></a>
 </div>
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <div class="card card-raised card-form-horizontal">
-            <div class="card-content">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="picture">
-                            <img src="<?= $tickets[0]->banner ?>" alt="Thumbnail Image"  class="img-rounded img-responsive">
-                            <p><strong><?= $tickets[0]->name ?></strong></p>
-                            <p><?= $tickets[0]->description ?></p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <h5>Information</h5>
-                        <div class="social-feed">
-                            <div class="feed-line">
-                                <b>Cinema</b>
-                                <p><?= env('APP_NAME') ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Showtime</b>
-                                <p><?= $tickets[0]->started_at ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Seats</b>
-                                <p><?= $str_seats ?></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <h5>Payment</h5>
-                        <div class="social-feed">
-                            <div class="feed-line">
-                                <b>Ticket</b>
-                                <p><?= prettyMoney($tickets_price) ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Combo</b>
-                                <p><?= prettyMoney($combos_price) ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Total</b>
-                                <p><?= prettyMoney($total) ?></p>
-                            </div>
-                        </div>
-                    </div>
+            <?php include section('customer.order.order_information') ?>
+            <div class="card-footer">
+                <div class="text-center">
+                    <a style="" href="#pablo" class="btn btn-success btn-round">Pay with ATM Card<div class="ripple-container"></div></a>
+                    <a style="" href="#pablo" class="btn btn-success btn-round">Pay with International Card<div class="ripple-container"></div></a>
                 </div>
             </div>
         </div>
--- app/Views/customer/order/choose_seat.php
+++ app/Views/customer/order/choose_seat.php
@@ -47,51 +47,7 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <div class="card card-raised card-form-horizontal">
-            <div class="card-content">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="picture">
-                            <img src="<?= $tickets[0]->banner ?>" alt="Thumbnail Image"  class="img-rounded img-responsive">
-                            <p><strong><?= $tickets[0]->name ?></strong></p>
-                            <p><?= $tickets[0]->description ?></p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <h5>Information</h5>
-                        <div class="social-feed">
-                            <div class="feed-line">
-                                <b>Cinema</b>
-                                <p><?= env('APP_NAME') ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Showtime</b>
-                                <p><?= $tickets[0]->started_at ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Seats</b>
-                                <p><?= $str_seats ?></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <h5>Payment</h5>
-                        <div class="social-feed">
-                            <div class="feed-line">
-                                <b>Ticket</b>
-                                <p><?= prettyMoney($tickets_price) ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Combo</b>
-                                <p><?= prettyMoney($combos_price) ?></p>
-                            </div>
-                            <div class="feed-line">
-                                <b>Total</b>
-                                <p><?= prettyMoney($total) ?></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php include section('customer.order.order_information') ?>
         </div>
     </div>
 </div>
